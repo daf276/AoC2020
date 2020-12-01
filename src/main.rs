b31fc759df46c3d3b7586262207d02ae391b5ee9
Attila <daf276@gmx.de>
2020-12-02 00:29:54 +0100

    Performance improvements

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
+use itertools::iproduct;
 use itertools::Itertools;
-use std::time::{Duration, Instant};
 
 fn main() {
     let input: Vec<u32> = include_str!("../day1")
@@ -7,13 +7,8 @@ fn main() {
         .map(|line| line.parse::<u32>().unwrap())
         .collect();
 
-    let now = Instant::now();
-    println!("Product: {}", part1(&input));
-    println!("Elapsed µs: {}", now.elapsed().as_micros());
-
-    let now = Instant::now();
-    println!("Product: {}", part2(&input));
-    println!("Elapsed µs: {}", now.elapsed().as_micros());
+    println!("Product Part 1: {}", part1(&input));
+    println!("Product Part 2: {}", part2(&input));
 }
 
 fn part1(input: &Vec<u32>) -> u32 {
@@ -27,11 +22,16 @@ fn part1(input: &Vec<u32>) -> u32 {
 }
 
 fn part2(input: &Vec<u32>) -> u32 {
-    return input
+    let sum = input
         .iter()
         .tuple_combinations()
-        .filter(|(&a, &b, &c)| a + b + c == 2020)
-        .map(|(a, b, c)| a * b * c)
-        .next()
-        .unwrap();
+        .filter(|(&a, &b)| a + b < 2020)
+        .collect::<Vec<(&u32, &u32)>>();
+
+    for ((a, b), c) in iproduct!(sum, input) {
+        if a + b + c == 2020 {
+            return a * b * c;
+        }
+    }
+    return 0;
 }
