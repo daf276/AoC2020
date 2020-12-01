80ad8cf33ad9057adec1fd82ceb960403d4c5205
Attila <daf276@gmx.de>
2020-12-02 00:48:43 +0100

    Added black box to test code

--- src/lib.rs
+++ src/lib.rs
@@ -10,11 +10,11 @@ mod day1;
 #[bench]
 fn day1_part1(b: &mut Bencher) {
     let input = day1::read_input();
-    b.iter(|| day1::part1(&input))
+    b.iter(|| test::black_box(day1::part1(&input)))
 }
 
 #[bench]
 fn day1_part2(b: &mut Bencher) {
     let input = day1::read_input();
-    b.iter(|| day1::part2(&input))
+    b.iter(|| test::black_box(day1::part2(&input)))
 }
