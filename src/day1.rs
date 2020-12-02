8e9b642a9edc2912186d5000fa649f0202d18cc5
Attila <daf276@gmx.de>
2020-12-02 02:04:18 +0100

    Turned Part2 into a o(n^2) solution

--- src/day1.rs
+++ src/day1.rs
@@ -19,15 +19,25 @@ pub fn part1(input: &Vec<u32>) -> u32 {
 }
 
 pub fn part2(input: &Vec<u32>) -> u32 {
-    let sum = input
-        .iter()
-        .tuple_combinations()
-        .filter(|(&a, &b)| a + b < 2020)
-        .collect::<Vec<(&u32, &u32)>>();
+    let mut sorted = input.clone();
+    sorted.sort();
 
-    for ((a, b), c) in iproduct!(sum, input) {
-        if a + b + c == 2020 {
-            return a * b * c;
+    for i in 0..sorted.len() - 2 {
+        let mut j = i + 1;
+        let mut k = sorted.len() - 1;
+
+        let a = sorted[i];
+        while j < k {
+            let b = sorted[j];
+            let c = sorted[k];
+
+            if a + b + c == 2020 {
+                return a * b * c;
+            } else if a + b + c > 2020 {
+                k -= 1;
+            } else {
+                j += 1;
+            }
         }
     }
     return 0;
